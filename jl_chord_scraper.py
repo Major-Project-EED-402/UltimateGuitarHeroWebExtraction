607ab110af8fc16046568ee91adb1756af5690cf
akshatsabavat <akshat.sabavat@gmail.com>
2023-10-16 10:49:29 +0530

    Failed, processing chord data

--- jl_chord_scraper.py
+++ jl_chord_scraper.py
@@ -1,36 +1,41 @@
-
 import jl_io as io
-from bs4 import BeautifulSoup
-from functools import reduce
-import time, os, random, uuid, pandas as pd
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from bs4 import BeautifulSoup
+from functools import reduce
+import os
+import uuid
+import pandas as pd 
 from jl_song_data import SongData
 
 class ChordExtractor:
 
     def __init__(self, raw_html_output_directory):
         self.raw_html_output_directory = raw_html_output_directory
-        self.driver = self.instantiate_chrome_driver()
+        self.driver = self.create_chrome_driver()
         self.first_time = True
-
+        
         if not os.path.isdir(self.raw_html_output_directory):
             os.mkdir(self.raw_html_output_directory)
+            
+    def extract_song_data(self,url):
+        chords_spans = self.get_chord_spans(url)
+        
+        chords = [span.decode_contents() for span in chords_spans]
+        
+        song_uuid = str(uuid.uuid4())
+        with open(f"{self.raw_html_output_directory}/{song_uuid}.html", "w") as file: # De los datos,como del cerdo, se guarda todo.
+            file.write(self.driver.page_source )
     
-    def instantiate_chrome_driver(self):
-        driver = webdriver.Chrome()
-        return driver
+        info = {
+            "url":url,
+            "chords":chords,
+            "uuid":song_uuid
+        }
+        
+        return info
     
-    def execute_CTA_accept_cookies(self):
-        try:
-            CTA_button = self.driver.find_element(By.XPATH, '//button[contains(text(), "thanks")]')
-            CTA_button.click()
-        except:
-            print('cookies banner not found in site')
-
-    def get_chord_span_element(self,url):
+    def get_chord_spans(self,url):
         self.driver.get(url)
         
         if self.driver.page_source == '<html><head></head><body></body></html>':
@@ -42,44 +47,34 @@ class ChordExtractor:
 
         soup = BeautifulSoup(self.driver.page_source, 'lxml')
 
-        article = soup.findAll('article')[3]
+        article = soup.findAll('article')[3];
         
         return article.findAll('span', {"style":"color: rgb(0, 0, 0);"})
-        
-    def extract_song_data(self, url):
-        chords_span_elements = self.get_chord_span_element(url)
-
-        chords = [span.decode_contents() for span in chords_span_elements]
-        song_uuid = str(uuid.uuid4())
-
-        with open(f"{self.raw_html_output_directory}/{song_uuid}.html", "w") as file: # URL pattern needed to extract paginated data
-            file.write(self.driver.page_source )
+    
+    def click_on_accept_cookies(self):
+        try:
+            button = self.driver.find_element(By.XPATH ,'//button[contains(text(), "thanks")]')
+            button.click()
+        except:
+            print('cookies banner not found. Ignored')
             
-        info = {
-            "url":url,
-            "chords":chords,
-            "uuid":song_uuid
-        }
-        
-        return info
+    
+    def create_chrome_driver(self):
+        driver = webdriver.Chrome()
+        return driver
 
 class LinkExtractor:
     BASE_URL = "https://www.ultimate-guitar.com/explore?&type[]=Chords"
-
+    
     def __init__(self):
-        self.driver = self.initiate_chrome_driver()
+        self.driver = self.create_chrome_driver()
         self.first_time = True
     
-    def initiate_chrome_driver(self):
-        driver = webdriver.Chrome()
-        return driver
-    
-    def execute_CTA_accept_cookies(self):
-        try:
-            CTA_button = self.driver.find_element(By.XPATH, '//button[contains(text(), "thanks")]')
-            CTA_button.click()
-        except:
-            print('cookies banner not found in site')
+    def get_all_songs(self, genre, decade, first_page, last_page):
+        links = self.get_all_filter_song_links(genre['pattern'],decade['pattern'], first_page, last_page)
+        result = [ self.link_to_song_dict(link,genre,decade) for link in links]
+        
+        return result
     
     def link_to_song_dict(self,link, genre, decade):
         return {
@@ -88,6 +83,12 @@ class LinkExtractor:
             "genre": genre["name"],
             "decade": decade["name"]
         }
+    
+    def get_all_filter_song_links(self,genreFilter,decadeFilter, first_page, last_page):
+        list_of_list = [self.get_links_single_page(genreFilter, decadeFilter, f'&page={page}')
+                  for page in range(first_page,last_page + 1)]
+        
+        return reduce(lambda list1, list2: [*list1, *list2], list_of_list)
     
     def get_links_single_page(self,genreFilter,decadeFilter, pageFilter):
         self.driver.get(f'{self.BASE_URL}{genreFilter}{decadeFilter}{pageFilter}')
@@ -103,18 +104,17 @@ class LinkExtractor:
 
         return soup.findAll('a', {"class":"_2KJtL _1mes3 kWOod"})
     
-    def get_all_filter_song_links(self,genreFilter,decadeFilter, first_page, last_page):
-        list_of_list = [self.get_links_single_page(genreFilter, decadeFilter, f'&page={page}')
-                  for page in range(first_page,last_page + 1)]
-        
-        return reduce(lambda list1, list2: [*list1, *list2], list_of_list)
+    def click_on_accept_cookies(self):
+        try:
+            button = self.driver.find_element(By.XPATH ,'//button[contains(text(), "thanks")]')
+            button.click()
+        except:
+            print('cookies banner not found. Ignored')
+            
     
-    def get_all_songs(self, genre, decade, starting_page, ending_page):
-        links = self.get_all_filter_song_links(genre['pattern'],decade['pattern'], starting_page, ending_page)
-        result = [ self.link_to_song_dict(link,genre,decade) for link in links]
-        
-        return result
-
+    def create_chrome_driver(self):
+        driver = webdriver.Chrome()
+        return driver
 
 class ChordScraper:
     def __init__(self, linkExtractor, chordExtractor, genres, decades, song_data):
